cee01a71ffd393166c6bcaa047133316a8ff4b1b
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-20 15:26:36 +0100

    save for now

--- todo2/src/App.js
+++ todo2/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
-import {HomePage} from "./components/HomePage";
-import {AddTodo} from "./components/AddTodo";
-import {ListTodo} from "./components/ListTodo";
+import { HomePage } from "./components/HomePage";
+import { AddTodo } from "./components/AddTodo";
+import { ListTodo } from "./components/ListTodo";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi world!</h1>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/new" element={<AddTodo />} />
-        <Route path="/list" element={<ListTodo />} />
-      </Routes>
-      </BrowserRouter>
+        <h1>Hi world!</h1>
+    
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new" element={<AddTodo />} />
+          <Route path="/list" element={<ListTodo />} />
+        </Routes>
+    
     </div>
   );
 }
