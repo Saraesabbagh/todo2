73583c380e8aa5d08f7937012ab88da605933ecb
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-20 12:32:17 +0100

    set up home page

---
+++ todo2/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>Things to do List!</h1>
+      <h3>What would you like to do today?</h3>
+      <button>Check my list</button>
+      <button>Add Something to my list</button>
+    </div>
+  );
+};
